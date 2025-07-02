c50ae6954a6047549a0139abaae9581a879fcc13
chuuhtetnaing <chuuhtetnaing@gmail.com>
2025-07-02 19:17:49 +0700

    b

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -2,7 +2,6 @@ export PYTHONPATH=.:$PYTHONPATH
 ACC_CONFIG_FILE="configs/acc_configs/multi_default.yaml"
 export CUDA_VISIBLE_DEVICES="0"
 NUM_PROCESSES=2
-MASTER_PORT=29500
 
 OUTPUT_DIR="out"
 mkdir -p $OUTPUT_DIR
@@ -11,7 +10,7 @@ cat "$0" >> $OUTPUT_DIR/run_script.sh
 DATA_DIR=data/iHarmony4
 TEST_FILE=test.jsonl
 
-accelerate launch --config_file $ACC_CONFIG_FILE --num_processes $NUM_PROCESSES --main_process_port $MASTER_PORT \
+accelerate launch --config_file $ACC_CONFIG_FILE --num_processes $NUM_PROCESSES \
 scripts/inference/main.py \
     --pretrained_model_name_or_path "/kaggle/working/DiffHarmony/checkpoints/diffharmonry-checkpoint/base" \
     --pretrained_vae_model_name_or_path "/kaggle/working/DiffHarmony/checkpoints/diffharmonry-checkpoint/condition_vae" \
